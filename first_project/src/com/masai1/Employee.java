160014c2511ed5b6f44fbb1bfabcb359b4c17b8f
sureshjungade <101568481+sureshjungade@users.noreply.github.com>
2022-05-22 11:07:08 +0530

    1st solution

---
+++ first_project/src/com/masai1/Employee.java
@@ -0,0 +1,54 @@
+package com.masai1;
+
+public class Employee {
+
+	private int employeeId;
+	private String employeeName;
+	private double salary;
+    private double netSalary;
+   
+   
+    public int getEmployeeId() {
+    	return employeeId;
+	}
+
+
+    public String getEmployeeName() {
+    	return employeeName;
+	}
+   
+    public double getSalary() {
+    	return salary;
+    }
+
+    public double getNetSalary() {
+    	return netSalary;
+    }
+   
+    public void setEmployeeId(int employeeId) {
+    	this.employeeId = employeeId;
+    }
+
+
+    public void setEmployeeName(String employeeName) {
+    	this.employeeName = employeeName;
+    }
+
+
+    public void setSalary(double salary) {
+    	this.salary = salary;
+	}
+
+
+    public void setNetSalary(double netSalary) {
+    	this.netSalary = netSalary;
+    }
+
+// salary -(salary*pfpercentage/100);
+
+    public double calculateNetSalary(double pfpercentage) {
+    	double x = (getSalary()-(getSalary()*pfpercentage/100));
+    	return x;
+    }
+
+}
