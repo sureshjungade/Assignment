aa45d3e002bc68d0b3aa5574d0e7a0b7db8e81dc
sureshjungade <101568481+sureshjungade@users.noreply.github.com>
2022-06-10 14:34:01 +0530

    cloning

--- 16th day/src/com/masai_01_Q/Product.java
+++ 16th day/src/com/masai_01_Q/Product.java
@@ -1,7 +1,11 @@
 package com.masai_01_Q;
 
 public class Product {
-
+	int productId;
+	String productName;
+	double productPrice;
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
